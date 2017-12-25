27ebf4b32275f8621adf4329fdfc6f2917ff5a98
msuthar5 <msuthar@hawk.iit.edu>
2017-12-25 11:45:17 -0600

    cleaned up code

--- python-based/classifiers/decision-tree.py
+++ python-based/classifiers/decision-tree.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn import tree# Decision tree classifier
+from sklearn import tree # Decision tree classifier
 import graphviz
 import numpy as np
 import pandas as pd
@@ -18,10 +18,21 @@ data.columns = ['age', 'sex', 'tb', 'db', 'aap', 'sgpaa', 'sgoaa', 'tp', 'alb', 
 
 target = data.label
 
-# Split into training and test data with 80 : 20 split
-train_data, test_data = train_test_split(data,test_size=0.2)
-train_label = train_data.label
-test_label = test_data.label
+d = data
+ 
+ # Change sex to boolean so we have all numerical attributes
+for i in range(0, len(d)):
+    if d.iloc[i,1] == 'Male':
+        d.iloc[i,1] = np.int32(1)
+    else:
+        d.iloc[i,1] = np.int32(0)
+        
+
+# Convert sex field from type object to type int8 for DecisionTreeClassifier()
+d.iloc[:,1] = d.iloc[:,1].astype('int8')
+
+# Split into test and train data 80/20 split
+test_data, train_data = train_test_split(d,test_size=0.2)
 
 # verify that the test and train data are not mixed at all
 
@@ -35,30 +46,16 @@ else:
     print("Completed 80/20 split of training and test data")
     print("Data preprocessing complete")
 
-d = data
-
- # Change sex to boolean so we have all numerical attributes
-
-for i in range(0, len(d)):
-    if d.iloc[i,1] == 'Male':
-        d.iloc[i,1] = np.int32(1)
-    else:
-        d.iloc[i,1] = np.int32(0)
-d.iloc[:,1] = d.iloc[:,1].astype('int8')
-
-#test_data, train_data = train_test_split(d,test_size=0.2)
 
 test_data_label = test_data.label
 train_data_label = train_data.label
 
 td = train_data.iloc[:,1:10]
-td
 
-# Define classifier
 classifier = tree.DecisionTreeClassifier()
 classifier.fit(td, train_data_label)
 
-dot_data = tree.export_graphviz(classifier, out_file=None)
-graph = graphviz.Source(dot_data)
-graph.render("ipld")
+dot_data = tree.export_graphviz(classifier, out_file=None) 
+graph = graphviz.Source(dot_data) 
+graph.render("ipld.gv")
 print(graph)
