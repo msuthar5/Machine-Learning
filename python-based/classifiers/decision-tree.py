0d4151efc226156daa2219da16a727ec7119b166
msuthar5 <msuthar@hawk.iit.edu>
2017-12-26 08:27:02 -0600

    determined classifier score

--- python-based/classifiers/decision-tree.py
+++ python-based/classifiers/decision-tree.py
@@ -18,22 +18,19 @@ data.columns = ['age', 'sex', 'tb', 'db', 'aap', 'sgpaa', 'sgoaa', 'tp', 'alb', 
 
 target = data.label
 
+# Copy df
 d = data
- 
- # Change sex to boolean so we have all numerical attributes
+
+# Change sex to boolean so we have all numerical attributes
 for i in range(0, len(d)):
     if d.iloc[i,1] == 'Male':
         d.iloc[i,1] = np.int32(1)
     else:
         d.iloc[i,1] = np.int32(0)
         
-
-# Convert sex field from type object to type int8 for DecisionTreeClassifier()
-d.iloc[:,1] = d.iloc[:,1].astype('int8')
-
 # Split into test and train data 80/20 split
-test_data, train_data = train_test_split(d,test_size=0.2)
-
+train_data, test_data = train_test_split(d,test_size=0.2)
+        
 # verify that the test and train data are not mixed at all
 
 s1 = set(list(test_data.index))
@@ -45,12 +42,22 @@ if len(intersection) != 0:
 else:
     print("Completed 80/20 split of training and test data")
     print("Data preprocessing complete")
-
+    
 
 test_data_label = test_data.label
 train_data_label = train_data.label
 
-td = train_data.iloc[:,1:10]
+td = train_data.iloc[:,0:10]
+
+td.iloc[:,8] = td.iloc[:,8].astype('float16')
+
+# Check for missing values
+print(td.isnull().any())
+
+print(td.ag.describe())
+
+# Replace missing values with average for column
+td.iloc[[15, 89, 264, 286],9] = 0.952637
 
 classifier = tree.DecisionTreeClassifier()
 classifier.fit(td, train_data_label)
@@ -58,4 +65,6 @@ classifier.fit(td, train_data_label)
 dot_data = tree.export_graphviz(classifier, out_file=None) 
 graph = graphviz.Source(dot_data) 
 graph.render("ipld.gv")
-print(graph)
+#print(graph)
+
+print(classifier.predict(test_data.iloc[:,0:10]))
